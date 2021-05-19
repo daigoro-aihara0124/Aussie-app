3dc55a413093b4c5cbe181a3c601cad1cdd39ce7
相原大介 <aiharadaisuke@aiharadaisukenoMacBook-Air.local>
2021-05-19 23:50:24 +0900

    編集機能続き、@babel/preset-stage-2を追加

---
+++ front/src/views/SchoolEdit.vue
@@ -0,0 +1,120 @@
+<template>
+  <div>
+    <h3>こちらは、{{ post.name }}さんの編集ページです。</h3>
+    <h3>学校を編集する</h3>
+      <label for="image">画像：</label>
+      <input
+      type="file"
+      id="image"
+      name="image"
+      accept="image/*"
+      @change="setImage"
+      >
+    <br><br>
+    <label for="address">場所：</label>
+    <input
+      id="address"
+      v-model="post.address"
+    >
+    <br><br>
+    <label for="name">スクール名：</label>
+    <input
+      id="name"
+      type="text"
+      v-model="post.name"
+    >
+    <br><br>
+    <label for="comment">スクール紹介：</label>
+    <textarea
+      id="comment"
+      v-model="post.comment"
+    ></textarea>
+    <br><br>
+    <label for="term">期間：</label>
+    <input
+      id="term"
+      v-model="post.term"
+    >
+    <br><br>
+    <label for="fee">料金：</label>
+    <input
+      id="fee"
+      v-model="post.fee"
+    >
+    <br><br>
+    <button @click="editInfo(post.id)">内容を編集する</button>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      id: this.$route.params.id,
+      imageFile: null,
+      post: {
+        id: '',
+        address: '',
+        name: '',
+        comment: '',
+        term: '',
+        fee: '',
+      }
+    };
+  },
+  mounted: function() {
+    this.setPostEdit(this.id);
+  },
+  methods: {
+    setPostEdit(id){
+      axios.get(`api/v1/posts/${id}`).then(res => {
+        this.post.image_url = res.data.image_url;
+        this.post.id = res.data.id;
+        this.post.address = res.data.address;
+        this.post.name = res.data.name;
+        this.post.comment = res.data.comment;
+        this.post.term = res.data.term;
+        this.post.fee = res.data.fee;
+      });
+    },
+    setImage(e) {
+      e.preventDefault();
+      this.imageFile = e.target.files[0];
+    },
+    async editInfo(id) {
+      let formData = new FormData();
+      formData.append("image", this.imageFile);
+      formData.append("id", this.post.id);
+      formData.append("address", this.post.address);
+      formData.append("name", this.post.name);
+      formData.append("comment", this.post.comment);
+      formData.append("term", this.post.term);
+      formData.append("fee", this.post.fee);
+      await axios.put(`api/v1/posts/${id}`, formData, {
+        headers: {
+          'content-type': 'multipart/form-data',
+        }
+      })
+      .then(() => {
+        this.$router.push('/schoolIndex');
+      }, (error) => {
+        console.log(error);
+      });
+    }
+  }
+};
+</script>
+
+
+<style scoped>
+li {
+  list-style:none;
+}
+
+.text_top {
+   vertical-align: top;
+ }
+
+</style>
