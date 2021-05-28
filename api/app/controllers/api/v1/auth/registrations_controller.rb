487156f61cb978c9b3b06d087a5d0274a455a9fd
相原大介 <aiharadaisuke@aiharadaisukenoMacBook-Air.local>
2021-05-28 18:35:54 +0900

    deviseのコントローラを編集

--- api/app/controllers/api/v1/auth/registrations_controller.rb
+++ api/app/controllers/api/v1/auth/registrations_controller.rb
@@ -1,32 +1,10 @@
 class Api::V1::Auth::RegistrationsController < DeviseTokenAuth::RegistrationsController
-
-  def update
-    if @resource
-      if params[:image] == 'null'
-        if @resource.send(resource_update_method, account_update_params_not_image)
-         yield @resource if block_given?
-         render_update_success
-         else
-          render_update_error
-        end
-      else
-        if @resource.send(resource_update_method, account_update_params)
-          yield @resource if block_given?
-          render_update_success
-        else
-          render_update_error
-        end
-      end
-    else
-      render_update_error_user_not_found
-    end
-  end
-
-  def read(*args)
-    @tempfile.read(*args)
-  end
-
+  
   private
+
+  def whoami
+    render json: current_api_v1_user, status: :ok
+  end
 
   def sign_up_params
     params.permit(:name, :email, :password)
