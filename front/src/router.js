274302549009458fa6dba7b02f5d8bbd17217f0d
相原大介 <aiharadaisuke@aiharadaisukenoMacBook-Air.local>
2021-07-15 23:57:31 +0900

    ページ遷移時にtopに移動するように編集

--- front/src/router.js
+++ front/src/router.js
@@ -178,5 +178,13 @@ export default new VueRouter({
         }
       }
     }
-  ]
+  ],
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition
+    }
+    else {
+      return { x: 0, y: 0 }
+    }
+  }
 });
